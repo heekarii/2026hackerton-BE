0d74b3732c79356b70444e8555736108977fdd31
damin <daminara0921-boop@users.noreply.github.com>
2026-06-24 14:53:48 +0900

    fix: register department routing endpoints

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from api.openai_analysis_endpoint import router as openai_router
 
 from routers.auth import router as auth_router
 from routers.complaints import router as complaints_router
+from routers.departments import router as departments_router
 
 
 app = FastAPI(
@@ -38,6 +39,7 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(complaints_router)
+app.include_router(departments_router)
 
 
 @app.get("/", tags=["Root"])
